507fc4e4cf62ca60d893e139437b0ee3cf35d05a
Luke Hamilton <luke@cloud.com>
2016-07-17 00:48:32 -0400

    nearly done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,20 @@
-// $('body').flowtype({
-//    minimum   : 500,
-//    maximum   : 1200,
-//    minFont   : 12,
-//    maxFont   : 40,
-//    fontRatio : 30
-// });
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
+
+// scroll to
+$(function () {
+  $('[data-scroll-to]').click(function (e) {
+    var el = e.currentTarget,
+      $targetEl = $($(el).attr('href')),
+      // offset = 150,
+      offset = 0,
+      targetElTop;
+    if ($targetEl.size()) {
+      targetElTop = $targetEl.offset().top;
+      $('html, body').animate({scrollTop: targetElTop - offset}, 500);
+    }
+    return false;
+  });
+});
